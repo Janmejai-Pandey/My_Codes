05a2c86b35c4be44af4d5058e26ec0aec1bd2044
Janmejai-Pandey <janmejai.pandey0403@gmail.com>
2025-09-24 15:42:13 +0530

    Tutorial 8 Programs Added

--- C & C++/Tutorials/Tutorial-8/1.c
+++ C & C++/Tutorials/Tutorial-8/1.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
-#include<string.h>
 
 int main(){
     char str[100];
     printf("Enter a string ( Max 100 characters ) - ");
     fgets(str, sizeof(str), stdin);
-    int n = strlen(str);
-    str[n - 1] = str[n - 1] == '\n' ? str[n - 1] = '\0' : str[n - 1];
-    n--;
 
     printf("Enter a character to delete - ");
     char ch;
@@ -16,18 +12,14 @@ int main(){
     char *ptr = str;
     while (*ptr != '\0') {
         if (*ptr == ch) {
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; *(ptr+i)!='\0'; i++) {
                 *(ptr + i) = *(ptr + i + 1);
             }
-            n--;
-            *(ptr + n) = '\0';
         } else {
             ptr++;
         }
     }
-    {
-        /* code */
-    }
-    
+    printf("String after deleting - ");
+    fputs(str, stdout);
     return 0;
 }
--- C & C++/Tutorials/Tutorial-8/2.c
+++ C & C++/Tutorials/Tutorial-8/2.c
@@ -1,10 +1,22 @@
 #include<stdio.h>
 
 int main(){
-    int num;
-    printf("Enter number - ");
-    scanf("%d",&num);
-
+    int num1;
+    printf("Enter 1st number- ");
+    scanf("%d",&num1);
     
+    int num2;
+    printf("Enter 2nd number- ");
+    scanf("%d",&num2);
+    printf("\n");
+
+    int *ptr1 = &num1;
+    int *ptr2 = &num2;
+
+    int temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+
+    printf("After swapping- \n1st number- %d\n2nd number- %d",num1,num2);
     return 0;
 }
